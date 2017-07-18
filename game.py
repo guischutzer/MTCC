4a1d998075d2a643f5f0ab6af6c0167bf3d83204
Guilherme Schützer <guischutzer@gmail.com>
2017-07-18 16:02:50 -0300

    Disfavored database.py, initiated game structure

--- game.py
+++ game.py
@@ -10,16 +10,26 @@ parser.add_argument('deck2',
                     help='Deck file (player 2)')
 
 args = parser.parse_args()
+db = DataBase()
 
 class Game:
 
-    def __init__(self, deck1, deck2, dbFilename='default.db'):
+    def __init__(self, deck1, deck2):
         self.player_1 = Player(1)
         self.player_2 = Player(2)
 
-        self.db = DataBase(dbFilename)
         player_1.setLibrary(readDeck(deck1))
         player_2.setLibrary(readDeck(deck2))
+
+        self.pqueue = []
+        activePlayer = randrange(1, 3)
+        setActivePlayer(activePlayer)
+
+        keep = [False, False]
+        while not all(keep):
+            keep = [pqueue[0].mulligan(), pqueue[1].mulligan()]
+
+
 
     def readDeck(self, filename):
 
@@ -27,9 +37,30 @@ class Game:
         f = open(filename, 'r')
 
         deckList = f.readlines()
-        for item in deckList:
-            
+        for entry in deckList:
+            entry = entry.split(" ")
+            number = int(entry[0])
+            name = " ".join(entry[1:])
+            for i in range(number):
+                library.append(db.addCard(name))
 
+        return library
+
+    def changeActivePlayer(self):
+        self.player_1.setActive(!player_1.isActive())
+        self.player_2.setActive(!player_2.isActive())
+        self.pqueue = pqueue.reverse()
+
+    def setActivePlayer(self, activePlayer):
+        self.player_1.setActive(activePlayer == 1)
+        self.player_2.setActive(activePlayer == 2)
+
+        if activePlayer == 1:
+            pqueue.append(self.player_1)
+            pqueue.append(self.player_2)
+        else:
+            pqueue.append(self.player_2)
+            pqueue.append(self.player_1)
 
 class Player:
 
@@ -42,13 +73,14 @@ class Player:
     def setLibrary(self, library):
         self.library = library
 
-    def draw(self):
+    def draw(self, n=1):
         if (len(self.library) == 0):
             self.lose = True
             return
 
-        card = self.library.pop()
-        self.hand.append(card)
+        for i in range(n):
+            card = self.library.pop()
+            self.hand.append(card)
 
     def shuffle(self):
         random.shuffle(self.library)
@@ -58,3 +90,16 @@ class Player:
 
     def loseLife(self, x):
         self.gainLife(-x)
+
+    def setActive(self, b):
+        self.active = b
+
+    def isActive(self):
+        return self.active
+
+    def start(self):
+        shuffle()
+        draw(7)
+        self.keep = False
+
+    def mulligan(self):
